8d8f32a97dd0e3af420d658387be48b411411aef
Sho Hashimoto <hashimoto@shokai.org>
2013-10-03 14:42:38 +0900

    update samples  #4

--- sample/sample.rb
+++ sample/sample.rb
@@ -3,14 +3,16 @@ require "rubygems"
 $:.unshift File.expand_path '../lib', File.dirname(__FILE__)
 require 'em-rocketio-linda-client'
 
+url = ARGV.empty? ? "http://linda.shokai.org" : ARGV.shift
+
 EM::run do
-  client = EM::RocketIO::Linda::Client.new "http://linda.shokai.org"
+  client = EM::RocketIO::Linda::Client.new url
   ts = client.tuplespace["test_spae"]
 
   client.io.on :connect do
     puts "connect #{client.io.type} (#{client.io.session})"
 
-    ts.watch [1,2] do |tuple|
+    ts.watch [1,2] do |tuple, info|
       p tuple
     end
 

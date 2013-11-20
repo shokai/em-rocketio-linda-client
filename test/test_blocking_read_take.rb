47ac9a1693e8ae0a2a70fb48f2234d3073f00000
Sho Hashimoto <hashimoto@shokai.org>
2013-11-20 10:36:08 +0900

    fix test for blocking TupleSpace#list  #5

--- test/test_blocking_read_take.rb
+++ test/test_blocking_read_take.rb
@@ -71,30 +71,28 @@ class TestBlockingReadTake < MiniTest::Test
   end
 
   def test_blocking_list
-    ts_name = "ts_blocking_read_#{rand Time.now.to_i}"
-    _tuple1 = ["a", "b", "c"]
-    _tuple2 = ["a", "b"]
-    _tuple3 = ["a", "b", "c", 1234]
+    ts_name = "ts_blocking_list_#{rand Time.now.to_i}"
+    _tuple1 = ["blocking", "a", "b", "c"]
+    _tuple2 = ["blocking", "a", "b"]
+    _tuple3 = ["blocking", "a", "b", "c", 1234]
     _result1 = nil
     _result2 = nil
     EM::run do
       client = EM::RocketIO::Linda::Client.new App.url
       ts = client.tuplespace[ts_name]
       client.io.on :connect do
-        ts.write _tuple1
-        ts.write _tuple2
-        ts.write _tuple3
-
-        _result1 = ts.list ["a", "b"]
-        p _result1
-        _result2 = ts.list ["a", "b", "c"]
-        p _result2
+        EM::defer do
+          ts.write _tuple1
+          ts.write _tuple2
+          ts.write _tuple3
+          _result1 = ts.list ["blocking", "a", "b"]
+          _result2 = ts.list ["blocking", "a", "b", "c"]
+        end
       end
 
       EM::defer do
         50.times do
           sleep 0.1
-          puts "waiting list"
           break if _result2
         end
         EM::add_timer 1 do
